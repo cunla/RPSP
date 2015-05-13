70d3dedde0d5fe6ece3368162ef20a18a714b98e
morand3 <daniel.moran@emc.com>
2015-05-13 11:09:10 +0300

    began work on Vm data fetcher

---
+++ src/main/java/com/emc/rpsp/mgmt/VmData.java
@@ -0,0 +1,15 @@
+package com.emc.rpsp.mgmt;
+
+/**
+ * Created by morand3 on 5/13/2015.
+ */
+public class VmData {
+	private String vmId;
+	private String clusterId;
+	private String consistencyGroup;
+	private long time;
+
+	public long getTime() {
+		return time;
+	}
+}
