0d5594d4b6b171acdc85ce9560f6d9776d52f9af
unknown <morand3@isenmorand3l1c.corp.emc.com>
2015-08-30 10:59:13 +0300

    better annotation

--- src/main/java/com/emc/rpsp/config/auditing/Billable.java
+++ src/main/java/com/emc/rpsp/config/auditing/Billable.java
@@ -1,7 +1,12 @@
 package com.emc.rpsp.config.auditing;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by morand3 on 8/30/2015.
  */
-public @interface Billable {
+@Target(value = ElementType.METHOD) @Retention(RetentionPolicy.SOURCE) public @interface Billable {
 }
