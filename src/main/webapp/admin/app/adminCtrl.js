924bb7557996997a07a1bd31c75c6d0f05cd8b93
unknown <morand3@isenmorand3l1c.corp.emc.com>
2015-11-09 14:57:24 +0200

    Hosting icons locally + added json prettifier

--- src/main/webapp/admin/app/adminCtrl.js
+++ src/main/webapp/admin/app/adminCtrl.js
@@ -1,6 +1,6 @@
 app.controller('adminCtrl', [ 'userService','$scope', function (userService,$scope) {
     userService.getAdminData().then(function(res){
-        $scope.data=res;
+        $scope.data=JSON.stringify(res, null, ' ');
     })
 
 }])
