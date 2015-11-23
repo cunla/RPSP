f79ab149cf4efb7c3f823e9d627b7442a5a833a7
unknown <morand3@isenmorand3l1c.corp.emc.com>
2015-11-23 15:22:54 +0200

    writing audit entries

--- src/main/java/com/emc/rpsp/config/auditing/AuditRepository.java
+++ src/main/java/com/emc/rpsp/config/auditing/AuditRepository.java
@@ -1,12 +1,11 @@
 package com.emc.rpsp.config.auditing;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by morand3 on 11/16/2015.
  */
 @RepositoryRestResource(collectionResourceRel = "audit", path = "audit")
-public interface AuditRepository extends CrudRepository<AuditEntry, Long> {
+public interface AuditRepository extends JpaRepository<AuditEntry, Long> {
 }
