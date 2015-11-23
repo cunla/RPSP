f336e7e55c1021d5747d214b443a3ec9b4d36220
unknown <morand3@isenmorand3l1c.corp.emc.com>
2015-11-23 14:21:03 +0200

    updated to spring-boot 1.3.0

--- src/main/java/com/emc/rpsp/config/RepositoryConfig.java
+++ src/main/java/com/emc/rpsp/config/RepositoryConfig.java
@@ -4,14 +4,14 @@ import com.emc.rpsp.rpsystems.ClusterSettings;
 import com.emc.rpsp.rpsystems.SystemSettings;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 /**
  * Created by morand3 on 1/15/2015.
  */
-@Configuration public class RepositoryConfig extends RepositoryRestMvcConfiguration {
+@Configuration public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
     @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(SystemSettings.class);
         config.exposeIdsFor(ClusterSettings.class);
         // config.exposeIdsFor(VirtualMachineData.class);
