69e84f7c985dde78a8674d6ecfeebe15507f25bc
unknown <morand3@isenmorand3l1c.corp.emc.com>
2015-11-30 12:05:04 +0200

    fixed creating tables on both dbs

--- src/main/java/com/emc/rpsp/config/DatabaseConfiguration.java
+++ src/main/java/com/emc/rpsp/config/DatabaseConfiguration.java
@@ -33,8 +33,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 @Configuration @EnableTransactionManagement
-@EntityScan(basePackages = { "com.emc.rpsp.config.auditing", "com.emc.rpsp.accounts.domain",
-"com.emc.rpsp.users.domain", "com.emc.rpsp.rpsystems", "com.emc.rpsp.vms.domain" })
+//@EntityScan(basePackages = { "com.emc.rpsp.config.auditing", "com.emc.rpsp.accounts.domain",
+//"com.emc.rpsp.users.domain", "com.emc.rpsp.rpsystems", "com.emc.rpsp.vms.domain" })
 @EnableJpaRepositories(
 basePackages = { "com.emc.rpsp.rpsystems", "com.emc.rpsp.accounts.repository",
 "com.emc.rpsp.vms.repository", "com.emc.rpsp.users.repository" },
