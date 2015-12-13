562ffa50d2c987f036ca8ded7ea6757722fe9c6c
unknown <morand3@isenmorand3l1c.corp.emc.com>
2015-12-13 18:27:43 +0200

    began work on error-handling

--- src/main/webapp/app/error-handling/error-handler.js
+++ src/main/webapp/app/error-handling/error-handler.js
@@ -1,10 +1,47 @@
 var HEADER_NAME = 'RPSP-Handle-Errors-Generically';
 var specificallyHandleInProgress = false;
 var app = angular.module('home');
-app.controller('errorController',['$scope','$modalInstance','modalParams', function($scope,$modalInstance,modalParams){
+app
+    .controller('errorController', ['$scope', '$modalInstance', 'modalParams',
+        function ($scope, $modalInstance, modalParams) {
+            $scope.error = modalParams.data;
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            }
+        }]);
+(function () {
+
+    var showError = function (error) {
+        var display = JSON.stringify(error.data);
+        alert(display);
+        //var $injector = angular.injector(['app']);
+        //var $modal = $injector.get('$modal');
+        //$modal.open({
+        //    templateUrl: 'app/error-modal.html',
+        //    controller: 'errorController',
+        //    windowClass: 'error-modal',
+        //    resolve: {
+        //        modalParams : function () {
+        //            return error;
+        //        }
+        //    }
+        //});
+    };
+    var httpInterceptor = function ($provide, $httpProvider) {
 
 
-	$scope.cancel = function(){
-		$modalInstance.dismiss('cancel');
-	}
-}]);
+        $provide.factory('httpInterceptor', function ($q) {
+            return {
+                response: function (response) {
+                    return response || $q.when(response);
+                },
+                responseError: function (rejection) {
+                    showError(rejection);
+                    return $q.reject(rejection);
+                }
+            };
+        });
+        $httpProvider.interceptors.push('httpInterceptor');
+    };
+    app.config(httpInterceptor);
+}());
