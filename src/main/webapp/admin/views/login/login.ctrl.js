b0ad1088ff0eb0afb8e06a3b42d9797508e4f098
unknown <morand3@isenmorand3l1c.corp.emc.com>
2015-12-16 15:10:44 +0200

    admin console redesign

---
+++ src/main/webapp/admin/views/login/login.ctrl.js
@@ -0,0 +1,31 @@
+app.controller('LoginCtrl', ['$scope', '$mdSidenav', '$location', '$mdDialog',
+    function ($scope, $mdSidenav, $location, $mdDialog) {
+        $scope.user = {};
+        var alert;
+        $scope.goHome = function () {
+            $location.path('/app/systems');
+        };
+        $scope.login = function (user) {
+            $scope.user = user;
+            if ($scope.loginForm.$valid) {
+                $scope.goHome();
+            } else {
+                showAlert();
+            }
+
+        }
+
+        function showAlert() {
+            alert = $mdDialog.alert()
+                .title('Oops!')
+                .content('Please provide a valid User and Password.')
+                .ok('Close');
+            $mdDialog
+                .show(alert)
+                .finally(function () {
+                    alert = undefined;
+
+                });
+        }
+
+    }]);
