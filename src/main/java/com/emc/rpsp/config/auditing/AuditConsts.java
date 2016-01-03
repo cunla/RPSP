4cdc062ea5ea7350334c69dbf4d2bf5873941e0d
unknown <morand3@isenmorand3l1c.corp.emc.com>
2016-01-03 15:28:22 +0200

    added backup auditing consts

--- src/main/java/com/emc/rpsp/config/auditing/AuditConsts.java
+++ src/main/java/com/emc/rpsp/config/auditing/AuditConsts.java
@@ -1,41 +1,44 @@
 package com.emc.rpsp.config.auditing;
 
 
+public interface AuditConsts {
 
-public class AuditConsts {
-	
-	private AuditConsts() {
-	}
-	
-	public static final String CG = "cd";
-	public static final String VM = "vm";
-	public static final String GS = "gs";
-	public static final String COPY = "copy";
-	public static final String CLUSTER = "cluster";
-	public static final String ENABLE_DR_TEST = "Enable DR test";
-	public static final String DISABLE_DR_TEST = "Disable DR test";
-	public static final String DR_TEST_RESULT = "dr_test_result";
-	public static final String DISABLE_DR_TEST_RESULT = "disable_dr_test_result";
-	public static final String CREATE_BOOKMARK = "Create bookmark";
-	public static final String CREATE_BOOKMARK_RESULT = "create_bookmark_result";
-	public static final String BOOKMARK_PARAMS = "bookmark_params";
-	public static final String FAILOVER = "Failover";
-	public static final String FAILOVER_RESULT = "failover_result";
-	public static final String RECOVER_PRODUCTION = "Recover production";
-	public static final String RECOVER_PRODUCTION_RESULT = "recover_production_result";
-	public static final String LOGIN_ACTION = "Login";
-	public static final String LOGOUT_ACTION = "Logout";
-	public static final String LOGIN_LOGOUT_SUCCESS_RESULT = "OK";
-	public static final String LOGIN_LOGOUT_FAILURE_RESULT = "Failure";
-	public static final String CREATE_CG = "Create CG";
-	public static final String CREATE_CG_SUBJ_PARAMS = "create_cg_subj_params";
-	public static final String CREATE_CG_OBJ_PARAMS = "create_cg_obj_params";
-	public static final String CREATE_CG_RESULT = "create_cg_result";
-	public static final String ADD_VM_TO_CG = "Add VM to CG";
-	public static final String ADD_VM_TO_CG_PARAMS = "add_vm_to_cg_params";
-	public static final String ADD_VM_TO_CG_RESULT = "add_vm_to_cg_result";
-	public static final String REMOVE_VM_FROM_CG = "Remove VM from CG";
-	public static final String REMOVE_VM_FROM_CG_PARAMS = "remove_vm_from_cg_params";
-	public static final String REMOVE_VM_FROM_CG_RESULT = "remove_vm_from_cg_result";
-	
+    public static final String CG = "cd";
+    public static final String VM = "vm";
+    public static final String GS = "gs";
+    public static final String COPY = "copy";
+    public static final String CLUSTER = "cluster";
+    public static final String ENABLE_DR_TEST = "Enable DR test";
+    public static final String DISABLE_DR_TEST = "Disable DR test";
+    public static final String DR_TEST_RESULT = "dr_test_result";
+    public static final String DISABLE_DR_TEST_RESULT = "disable_dr_test_result";
+    public static final String CREATE_BOOKMARK = "Create bookmark";
+    public static final String CREATE_BOOKMARK_RESULT = "create_bookmark_result";
+    public static final String BOOKMARK_PARAMS = "bookmark_params";
+    public static final String FAILOVER = "Failover";
+    public static final String FAILOVER_RESULT = "failover_result";
+    public static final String RECOVER_PRODUCTION = "Recover production";
+    public static final String RECOVER_PRODUCTION_RESULT = "recover_production_result";
+    public static final String LOGIN_ACTION = "Login";
+    public static final String LOGOUT_ACTION = "Logout";
+    public static final String LOGIN_LOGOUT_SUCCESS_RESULT = "OK";
+    public static final String LOGIN_LOGOUT_FAILURE_RESULT = "Failure";
+    public static final String CREATE_CG = "Create CG";
+    public static final String CREATE_CG_SUBJ_PARAMS = "create_cg_subj_params";
+    public static final String CREATE_CG_OBJ_PARAMS = "create_cg_obj_params";
+    public static final String CREATE_CG_RESULT = "create_cg_result";
+    public static final String ADD_VM_TO_CG = "Add VM to CG";
+    public static final String ADD_VM_TO_CG_PARAMS = "add_vm_to_cg_params";
+    public static final String ADD_VM_TO_CG_RESULT = "add_vm_to_cg_result";
+    public static final String REMOVE_VM_FROM_CG = "Remove VM from CG";
+    public static final String REMOVE_VM_FROM_CG_PARAMS = "remove_vm_from_cg_params";
+    public static final String REMOVE_VM_FROM_CG_RESULT = "remove_vm_from_cg_result";
+
+    public static final String ENABLE_BACKUP_ACCESS = "Enable backup access";
+    public static final String DISABLE_BACKUP_ACCESS = "Disable backup access";
+    public static final String TASK_STATUS = "task_status";
+
+    public static final String TASK = "task";
+    String BACKUP_NAME = "backup_name";
+    String TASKS = "tasks";
 }
--- src/main/java/com/emc/rpsp/config/auditing/rp4vm/Rp4vmAuditTypesHandler.java
+++ src/main/java/com/emc/rpsp/config/auditing/rp4vm/Rp4vmAuditTypesHandler.java
@@ -1,19 +1,5 @@
 package com.emc.rpsp.config.auditing.rp4vm;
 
-import java.lang.reflect.Method;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import com.emc.rpsp.accounts.domain.Account;
 import com.emc.rpsp.config.auditing.AuditConsts;
 import com.emc.rpsp.config.auditing.AuditEntry;
@@ -23,36 +9,39 @@ import com.emc.rpsp.config.auditing.cache.AuditedMethodDescriptor;
 import com.emc.rpsp.config.auditing.cache.AuditingCache;
 import com.emc.rpsp.infra.common.auth.domain.AbstractCurrentUser;
 import com.emc.rpsp.users.service.UserService;
-import com.emc.rpsp.vmstructure.domain.AccountVmsStructure;
-import com.emc.rpsp.vmstructure.domain.ClusterDefinition;
-import com.emc.rpsp.vmstructure.domain.ConsistencyGroup;
-import com.emc.rpsp.vmstructure.domain.GroupSet;
-import com.emc.rpsp.vmstructure.domain.SystemInfo;
-import com.emc.rpsp.vmstructure.domain.VmContainer;
-import com.emc.rpsp.vmstructure.domain.VmDefinition;
+import com.emc.rpsp.vmstructure.domain.*;
 import com.emc.rpsp.vmstructure.service.AccountVmsStructureService;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.lang.reflect.Method;
+import java.util.*;
 
 /**
  * Created by morand3 on 11/25/2015.
  */
 public class Rp4vmAuditTypesHandler implements AuditTypesHandler {
-    @Autowired 
+    @Autowired
     private AuditRepository auditRepository;
-    @Autowired 
+    @Autowired
     private UserService userService;
-   
+
     @Autowired
     private AccountVmsStructureService accountVmsStructureService = null;
-    
-    private Map<Long, AccountVmsStructure> accountIdToStructureMap = 
-    		                      new HashMap<Long, AccountVmsStructure>();
-    
+
+    private Map<Long, AccountVmsStructure> accountIdToStructureMap =
+        new HashMap<Long, AccountVmsStructure>();
+
 
     public Rp4vmAuditTypesHandler() {
     }
 
     public AuditEntry getAuditEntry(Date date, String username, ProceedingJoinPoint joinPoint,
-        Object resultValue) {
+                                    Object resultValue) {
         AuditedMethodDescriptor descriptor = getAuditMethodDescriptor(joinPoint);
         String actionName = descriptor.getAction();
         Object[] args = joinPoint.getArgs();
@@ -100,66 +89,68 @@ public class Rp4vmAuditTypesHandler implements AuditTypesHandler {
     }
 
     @SuppressWarnings("unchecked")
-	public String getAuditText(String paramType, Object paramValue) {
+    public String getAuditText(String paramType, Object paramValue) {
         if (AuditConsts.CG.equals(paramType)) {
             return "Group: " + getCgName(paramValue);
-        } else if (AuditConsts.VM.equals(paramType)) {
+        }
+        if (AuditConsts.VM.equals(paramType)) {
             return "VM: " + getVmName(paramValue);
-        } else if (AuditConsts.GS.equals(paramType)) {
+        }
+        if (AuditConsts.GS.equals(paramType)) {
             return "GroupSet: " + getGroupSetName(paramValue);
         }
-        else if (AuditConsts.COPY.equals(paramType)){
-        	return getCopyName(paramValue);
+        if (AuditConsts.COPY.equals(paramType)) {
+            return getCopyName(paramValue);
         }
-        else if (AuditConsts.CLUSTER.equals(paramType)){
-        	return "Cluster: " + getClusterName(paramValue);
+        if (AuditConsts.CLUSTER.equals(paramType)) {
+            return "Cluster: " + getClusterName(paramValue);
         }
-        else if (AuditConsts.BOOKMARK_PARAMS.equals(paramType)){
-        	return "Bookmark: " + ((Map<String, String>)paramValue).get("name");
+        if (AuditConsts.BOOKMARK_PARAMS.equals(paramType)) {
+            return "Bookmark: " + ((Map<String, String>) paramValue).get("name");
         }
-        else if (AuditConsts.ADD_VM_TO_CG_PARAMS.equals(paramType)){
-        	return "VM: " + getVmName(((Map<String, String>)paramValue).get("id"));
+        if (AuditConsts.ADD_VM_TO_CG_PARAMS.equals(paramType)) {
+            return "VM: " + getVmName(((Map<String, String>) paramValue).get("id"));
         }
-        else if (AuditConsts.CREATE_CG_SUBJ_PARAMS.equals(paramType)){
-        	Map<String, Object> params = (Map<String, Object>)paramValue;
-        	return "Group: " + params.get("groupName").toString();
+        if (AuditConsts.CREATE_CG_SUBJ_PARAMS.equals(paramType)) {
+            Map<String, Object> params = (Map<String, Object>) paramValue;
+            return "Group: " + params.get("groupName").toString();
         }
-        else if (AuditConsts.CREATE_CG_OBJ_PARAMS.equals(paramType)){
-        	Map<String, Object> params = (Map<String, Object>)paramValue;
-        	List<Object> vmIdsObj = (List<Object>)params.get("vms");
-        	List<String> vmIds = new LinkedList<String>();
-    		vmIdsObj.forEach((obj) -> vmIds.add(String.valueOf(obj)));
-    		String res = "VMS:";
-    		for(String vmId : vmIds){
-    			res += " " + getVmName(vmId);
-    		}
-        	return res;
+        if (AuditConsts.CREATE_CG_OBJ_PARAMS.equals(paramType)) {
+            Map<String, Object> params = (Map<String, Object>) paramValue;
+            List<Object> vmIdsObj = (List<Object>) params.get("vms");
+            List<String> vmIds = new LinkedList<String>();
+            vmIdsObj.forEach((obj) -> vmIds.add(String.valueOf(obj)));
+            String res = "VMS:";
+            for (String vmId : vmIds) {
+                res += " " + getVmName(vmId);
+            }
+            return res;
         }
-        else if(AuditConsts.DR_TEST_RESULT.equals(paramType) 
-        		|| AuditConsts.DISABLE_DR_TEST_RESULT.equals(paramType)
-        		|| AuditConsts.CREATE_BOOKMARK_RESULT.equals(paramType)
-        		|| AuditConsts.FAILOVER_RESULT.equals(paramType)
-        		|| AuditConsts.RECOVER_PRODUCTION_RESULT.equals(paramType)
-        		|| AuditConsts.CREATE_CG_RESULT.equals(paramType)
-        		|| AuditConsts.ADD_VM_TO_CG_RESULT.equals(paramType)
-        		|| AuditConsts.REMOVE_VM_FROM_CG_RESULT.equals(paramType)){
-        	ResponseEntity<HttpStatus> response = (ResponseEntity<HttpStatus>)paramValue;
-        	return response.getStatusCode().name();
+        if (AuditConsts.DR_TEST_RESULT.equals(paramType)
+            || AuditConsts.DISABLE_DR_TEST_RESULT.equals(paramType)
+            || AuditConsts.CREATE_BOOKMARK_RESULT.equals(paramType)
+            || AuditConsts.FAILOVER_RESULT.equals(paramType)
+            || AuditConsts.RECOVER_PRODUCTION_RESULT.equals(paramType)
+            || AuditConsts.CREATE_CG_RESULT.equals(paramType)
+            || AuditConsts.ADD_VM_TO_CG_RESULT.equals(paramType)
+            || AuditConsts.REMOVE_VM_FROM_CG_RESULT.equals(paramType)) {
+            ResponseEntity<HttpStatus> response = (ResponseEntity<HttpStatus>) paramValue;
+            return response.getStatusCode().name();
         }
-        else{
-        	return paramType + paramValue;
-        }
+
+        return paramType + paramValue;
+
         //throw new RpspAuditException("Couldn't get text for audit type {" + paramType + "}");
     }
 
     @Override
     public void writeRecordToAudit(Date date, String username, ProceedingJoinPoint joinPoint,
-        Object resultValue) {
+                                   Object resultValue) {
         AuditEntry entry = getAuditEntry(date, username, joinPoint, resultValue);
         auditRepository.save(entry);
         auditRepository.flush();
     }
-    
+
     @Override
     public void writeRecordToAudit(AuditEntry entry) {
         auditRepository.save(entry);
@@ -176,178 +167,174 @@ public class Rp4vmAuditTypesHandler implements AuditTypesHandler {
         }
         return null;
     }*/
-    
-    private AccountVmsStructure getAccountVmsStructure(){
-    	AccountVmsStructure res = null;
-    	AbstractCurrentUser user = userService.findCurrentUser();
+
+    private AccountVmsStructure getAccountVmsStructure() {
+        AccountVmsStructure res = null;
+        AbstractCurrentUser user = userService.findCurrentUser();
         Account account = user.getAccount();
-        if(accountIdToStructureMap.get(account.getId()) == null){
-        	updateAccountVmsStructure();
+        if (accountIdToStructureMap.get(account.getId()) == null) {
+            updateAccountVmsStructure();
         }
         res = accountIdToStructureMap.get(account.getId());
         return res;
     }
-  
-    
-    private void updateAccountVmsStructure(){
-    	AbstractCurrentUser user = userService.findCurrentUser();
+
+
+    private void updateAccountVmsStructure() {
+        AbstractCurrentUser user = userService.findCurrentUser();
         Account account = user.getAccount();
-    	AccountVmsStructure accountVmsStructure = accountVmsStructureService.getAccountVmsStrucure();
-    	accountIdToStructureMap.put(account.getId(), accountVmsStructure);
-    }
-    
-    
-    private String getGroupSetName(Object groupSetId){
-    	String groupSetName = null;
-    	GroupSet groupSet = getGroupSetData(groupSetId);
-    	if(groupSet == null){
-    		updateAccountVmsStructure();
-    	}
-    	groupSet = getGroupSetData(groupSetId);
-    	if(groupSet != null){
-    		groupSetName = groupSet.getName();
-    	}
-    	return groupSetName;
+        AccountVmsStructure accountVmsStructure = accountVmsStructureService.getAccountVmsStrucure();
+        accountIdToStructureMap.put(account.getId(), accountVmsStructure);
     }
 
-    
-    
-    private GroupSet getGroupSetData(Object groupSetId) {    	
-    	AccountVmsStructure accountVmsStructure = getAccountVmsStructure();
-        for(VmContainer currVmContainer : accountVmsStructure.getProtectedVms()){        	
-        	if(currVmContainer instanceof GroupSet){
-        		GroupSet currGs = (GroupSet)currVmContainer;
-        		if(currGs.getId().equals(groupSetId.toString())){
-        			return currGs;
-        		}
-        	}		  		
-    	}
+
+    private String getGroupSetName(Object groupSetId) {
+        String groupSetName = null;
+        GroupSet groupSet = getGroupSetData(groupSetId);
+        if (groupSet == null) {
+            updateAccountVmsStructure();
+        }
+        groupSet = getGroupSetData(groupSetId);
+        if (groupSet != null) {
+            groupSetName = groupSet.getName();
+        }
+        return groupSetName;
+    }
+
+
+    private GroupSet getGroupSetData(Object groupSetId) {
+        AccountVmsStructure accountVmsStructure = getAccountVmsStructure();
+        for (VmContainer currVmContainer : accountVmsStructure.getProtectedVms()) {
+            if (currVmContainer instanceof GroupSet) {
+                GroupSet currGs = (GroupSet) currVmContainer;
+                if (currGs.getId().equals(groupSetId.toString())) {
+                    return currGs;
+                }
+            }
+        }
         return null;
     }
-    
-    
-    private String getVmName(Object vmId){
-    	String vmName = null;
-    	VmDefinition vmDefinition = getVmData(vmId);
-    	if(vmDefinition == null){
-    		updateAccountVmsStructure();
-    	}
-    	vmDefinition = getVmData(vmId);
-    	if(vmDefinition != null){
-    		vmName = vmDefinition.getName();
-    	}
-    	return vmName;
+
+
+    private String getVmName(Object vmId) {
+        String vmName = null;
+        VmDefinition vmDefinition = getVmData(vmId);
+        if (vmDefinition == null) {
+            updateAccountVmsStructure();
+        }
+        vmDefinition = getVmData(vmId);
+        if (vmDefinition != null) {
+            vmName = vmDefinition.getName();
+        }
+        return vmName;
     }
-    
+
     private VmDefinition getVmData(Object vmId) {
-    	AccountVmsStructure accountVmsStructure = getAccountVmsStructure();
-    	for(VmDefinition currVmdeDefinition : accountVmsStructure.getUnprotectedVms()){
-    		if(currVmdeDefinition.getId().equals(vmId)){
-    			return currVmdeDefinition;
-    		}
-    	}
-    	for(VmContainer currVmContainer : accountVmsStructure.getProtectedVms()){
-    		//CG
-    		if(currVmContainer instanceof ConsistencyGroup){
-    			List<VmDefinition> vms = ((ConsistencyGroup)currVmContainer).getVms();
-    			for(VmDefinition currVmdeDefinition : vms){
-    				if(currVmdeDefinition.getId().equals(vmId)){
-    					return currVmdeDefinition;
-    				}
-    			}
-    		}
-    		else{
-    			GroupSet currGs = (GroupSet)currVmContainer;
-    			List<VmContainer> groups = currGs.getConsistencyGroups();
-    			for(VmContainer currCg : groups){
-    				List<VmDefinition> vms = ((ConsistencyGroup)currCg).getVms();
-    				for(VmDefinition currVmdeDefinition : vms){
-        				if(currVmdeDefinition.getId().equals(vmId)){
-        					return currVmdeDefinition;
-        				}
-        			}
-    			}
-    		}
-    		
-    	}
+        AccountVmsStructure accountVmsStructure = getAccountVmsStructure();
+        for (VmDefinition currVmdeDefinition : accountVmsStructure.getUnprotectedVms()) {
+            if (currVmdeDefinition.getId().equals(vmId)) {
+                return currVmdeDefinition;
+            }
+        }
+        for (VmContainer currVmContainer : accountVmsStructure.getProtectedVms()) {
+            //CG
+            if (currVmContainer instanceof ConsistencyGroup) {
+                List<VmDefinition> vms = ((ConsistencyGroup) currVmContainer).getVms();
+                for (VmDefinition currVmdeDefinition : vms) {
+                    if (currVmdeDefinition.getId().equals(vmId)) {
+                        return currVmdeDefinition;
+                    }
+                }
+            } else {
+                GroupSet currGs = (GroupSet) currVmContainer;
+                List<VmContainer> groups = currGs.getConsistencyGroups();
+                for (VmContainer currCg : groups) {
+                    List<VmDefinition> vms = ((ConsistencyGroup) currCg).getVms();
+                    for (VmDefinition currVmdeDefinition : vms) {
+                        if (currVmdeDefinition.getId().equals(vmId)) {
+                            return currVmdeDefinition;
+                        }
+                    }
+                }
+            }
+
+        }
         return null;
     }
-    
-    
-    private String getCgName(Object cgId){
-    	String cgName = null;
-    	ConsistencyGroup cg = getCgData(cgId);
-    	if(cg == null){
-    		updateAccountVmsStructure();
-    	}
-    	cg = getCgData(cgId);
-    	if(cg != null){
-    		cgName = cg.getName();
-    	}
-    	return cgName;
+
+
+    private String getCgName(Object cgId) {
+        String cgName = null;
+        ConsistencyGroup cg = getCgData(cgId);
+        if (cg == null) {
+            updateAccountVmsStructure();
+        }
+        cg = getCgData(cgId);
+        if (cg != null) {
+            cgName = cg.getName();
+        }
+        return cgName;
     }
 
-    
+
     private ConsistencyGroup getCgData(Object cgId) {
-    	AccountVmsStructure accountVmsStructure = getAccountVmsStructure();
-    	
-    	for(VmContainer currVmContainer : accountVmsStructure.getProtectedVms()){
-    		//CG
-    		if(currVmContainer instanceof ConsistencyGroup){
-        		ConsistencyGroup currCg = (ConsistencyGroup)currVmContainer;
-        		if(currCg.getId().equals(cgId.toString())){
-        			return currCg;
-        		}	
-    		}
-    		//Nested CG
-    		else{
-    			GroupSet currGs = (GroupSet)currVmContainer;
-    			List<VmContainer> groups = currGs.getConsistencyGroups();
-    			for(VmContainer currCg : groups){
-    				ConsistencyGroup currNestedCg = (ConsistencyGroup)currCg;
-    				if(currNestedCg.getId().equals(cgId.toString())){
-            			return currNestedCg;
-            		}
-    			}
-    		}
-    		
-    	}
+        AccountVmsStructure accountVmsStructure = getAccountVmsStructure();
+
+        for (VmContainer currVmContainer : accountVmsStructure.getProtectedVms()) {
+            //CG
+            if (currVmContainer instanceof ConsistencyGroup) {
+                ConsistencyGroup currCg = (ConsistencyGroup) currVmContainer;
+                if (currCg.getId().equals(cgId.toString())) {
+                    return currCg;
+                }
+            }
+            //Nested CG
+            else {
+                GroupSet currGs = (GroupSet) currVmContainer;
+                List<VmContainer> groups = currGs.getConsistencyGroups();
+                for (VmContainer currCg : groups) {
+                    ConsistencyGroup currNestedCg = (ConsistencyGroup) currCg;
+                    if (currNestedCg.getId().equals(cgId.toString())) {
+                        return currNestedCg;
+                    }
+                }
+            }
+
+        }
         return null;
     }
-    
-    
-    private String getClusterName(Object clusterId){
-    	String clusterName = null;
-    	ClusterDefinition clusterDefinition = getClusterData(clusterId);
-    	if(clusterDefinition == null){
-    		updateAccountVmsStructure();
-    	}
-    	clusterDefinition = getClusterData(clusterId);
-    	if(clusterDefinition != null){
-    		clusterName = clusterDefinition.getName();
-    	}
-    	return clusterName;
-    }
-    
-    
-    private ClusterDefinition getClusterData(Object clusterId) {
-    	AccountVmsStructure accountVmsStructure = getAccountVmsStructure();
-    	SystemInfo sysInfo = accountVmsStructure.getSystemInfo();
-    	if(sysInfo.getProductionCluster().getId().equals(clusterId.toString())){
-    		return sysInfo.getProductionCluster();
-    	}
-    	else{
-    		for(ClusterDefinition currReplicaCluster : sysInfo.getReplicaClusters()){
-    			if(currReplicaCluster.getId().equals(clusterId.toString())){
-    	    		return currReplicaCluster;
-    	    	}
-    		}
-    	}
-    	return null;
-    }
-    
 
-    
+
+    private String getClusterName(Object clusterId) {
+        String clusterName = null;
+        ClusterDefinition clusterDefinition = getClusterData(clusterId);
+        if (clusterDefinition == null) {
+            updateAccountVmsStructure();
+        }
+        clusterDefinition = getClusterData(clusterId);
+        if (clusterDefinition != null) {
+            clusterName = clusterDefinition.getName();
+        }
+        return clusterName;
+    }
+
+
+    private ClusterDefinition getClusterData(Object clusterId) {
+        AccountVmsStructure accountVmsStructure = getAccountVmsStructure();
+        SystemInfo sysInfo = accountVmsStructure.getSystemInfo();
+        if (sysInfo.getProductionCluster().getId().equals(clusterId.toString())) {
+            return sysInfo.getProductionCluster();
+        } else {
+            for (ClusterDefinition currReplicaCluster : sysInfo.getReplicaClusters()) {
+                if (currReplicaCluster.getId().equals(clusterId.toString())) {
+                    return currReplicaCluster;
+                }
+            }
+        }
+        return null;
+    }
+
+
     private String getCopyName(Object copyId) {
         return "Copy: " + copyId;
     }
