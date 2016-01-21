e002c286915aa4deef59427060c04d535cc19bb8
Boris Shpilyuck <boris.shpilyuck@emc.com>
2016-01-21 18:31:37 +0200

    Virtual config API - systems/clusters updated with virtual conf info

--- src/main/java/com/emc/rpsp/rpsystems/ClusterSettings.java
+++ src/main/java/com/emc/rpsp/rpsystems/ClusterSettings.java
@@ -1,6 +1,9 @@
 package com.emc.rpsp.rpsystems;
 
+import com.emc.rpsp.virtualconfig.domain.VcenterConfig;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
@@ -9,70 +12,98 @@ import javax.persistence.*;
 /**
  * Created by morand3 on 2/8/2015.
  */
-@Entity @Table(name = "T_CLUSTERS") @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Entity
+@Table(name = "T_CLUSTERS")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class ClusterSettings {
-    @Id @GeneratedValue(strategy = GenerationType.AUTO) private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    @Column private String country;
-    @Column private Long clusterId;
-    @Column private String clusterName;
-    @Column private String friendlyName;
+	@Column
+	private String country;
+	@Column
+	private Long clusterId;
+	@Column
+	private String clusterName;
+	@Column
+	private String friendlyName;
+	
+	@Transient
+	@JsonProperty
+	private VcenterConfig vcenterConfig;
 
-    @JsonIgnore 
-    @ManyToOne(fetch = FetchType.LAZY) 
-    private SystemSettings systemSettings;
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY)
+	private SystemSettings systemSettings;
 
-    public ClusterSettings() {
-    }
+	public ClusterSettings() {
+	}
 
-    public ClusterSettings(Long clusterId, String clusterName, SystemSettings systemSettings) {
-        this.clusterId = clusterId;
-        this.clusterName = clusterName;
-        this.systemSettings = systemSettings;
-    }
+	public ClusterSettings(Long clusterId, String clusterName,
+			SystemSettings systemSettings) {
+		this.clusterId = clusterId;
+		this.clusterName = clusterName;
+		this.systemSettings = systemSettings;
+	}
 
-    public String getCountry() {
-        return country;
-    }
+	public String getCountry() {
+		return country;
+	}
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
-    public Long getClusterId() {
-        return clusterId;
-    }
+	public Long getClusterId() {
+		return clusterId;
+	}
 
-    public void setClusterId(Long clusterId) {
-        this.clusterId = clusterId;
-    }
+	public void setClusterId(Long clusterId) {
+		this.clusterId = clusterId;
+	}
 
-    public String getClusterName() {
-        return clusterName;
-    }
+	public String getClusterName() {
+		return clusterName;
+	}
 
-    public void setClusterName(String clusterName) {
-        this.clusterName = clusterName;
-    }
+	public void setClusterName(String clusterName) {
+		this.clusterName = clusterName;
+	}
 
-    public String getFriendlyName() {
-        return friendlyName;
-    }
+	public String getFriendlyName() {
+		return friendlyName;
+	}
 
-    public void setFriendlyName(String friendlyName) {
-        this.friendlyName = friendlyName;
-    }
+	public void setFriendlyName(String friendlyName) {
+		this.friendlyName = friendlyName;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public SystemSettings getSystemSettings() {
-        return systemSettings;
-    }
+	public SystemSettings getSystemSettings() {
+		return systemSettings;
+	}
 
-    public void setSystemSettings(SystemSettings systemSettings) {
-        this.systemSettings = systemSettings;
-    }
+	public void setSystemSettings(SystemSettings systemSettings) {
+		this.systemSettings = systemSettings;
+	}
+
+	public VcenterConfig getVcenterConfig() {
+		return vcenterConfig;
+	}
+
+	public void setVcenterConfig(VcenterConfig vcenterConfig) {
+		this.vcenterConfig = vcenterConfig;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	
 
 }
--- src/main/java/com/emc/rpsp/rpsystems/SettingsResource.java
+++ src/main/java/com/emc/rpsp/rpsystems/SettingsResource.java
@@ -3,6 +3,9 @@ package com.emc.rpsp.rpsystems;
 import com.emc.rpsp.RpspException;
 import com.emc.rpsp.config.auditing.annotations.RpspAudited;
 import com.emc.rpsp.fal.Client;
+import com.emc.rpsp.virtualconfig.domain.VcenterConfig;
+import com.emc.rpsp.virtualconfig.service.VirtualConfigurationService;
+
 import org.apache.commons.httpclient.protocol.InetAddressUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -20,127 +24,159 @@ import java.util.Optional;
 /**
  * Created by morand3 on 1/29/2015.
  */
-@RestController @RequestMapping("/app") public class SettingsResource {
-    private final Logger log = LoggerFactory.getLogger(SettingsResource.class);
+@RestController
+@RequestMapping("/app")
+public class SettingsResource {
+	private final Logger log = LoggerFactory.getLogger(SettingsResource.class);
 
-    @Inject private SystemConnectionInfoRepository systemConnectionInfoRepository;
+	@Inject
+	private SystemConnectionInfoRepository systemConnectionInfoRepository;
+	
+	@Inject
+	private VirtualConfigurationService virtualConfigurationService;
 
-    @RpspAudited
-    @RequestMapping(value = "/rest/systems", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<SystemSettings>> findSystems() {
-        List<SystemSettings> systemsSettings = systemConnectionInfoRepository.findAll();
-        return new ResponseEntity<>(systemsSettings, HttpStatus.OK);
-    }
+	@RpspAudited
+	@RequestMapping(value = "/rest/systems", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<SystemSettings>> findSystems() {
+		List<SystemSettings> systemsSettings = systemConnectionInfoRepository
+				.findAll();
+		addVirtualConfigurationInfo(systemsSettings);
+		return new ResponseEntity<>(systemsSettings, HttpStatus.OK);
+	}
 
-   @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/rest/systems/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<SystemSettings> findSystem(@PathVariable("id") Long id) {
-        log.debug("Testing systemSettings with id {}", id);
-        SystemSettings systemSettings = systemConnectionInfoRepository.findOne(id);
-        return new ResponseEntity<>(systemSettings, HttpStatus.OK);
-    }
-    
-    /*@PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/rest/systems/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<SystemSettings> findSystem(@PathVariable("id") Long id) {
-        log.debug("Testing systemSettings with id {}", id);
-        SystemSettings systemSettings = new SystemSettings();
-        systemSettings.setIsDrttc(true);
-        systemSettings.setIsMultiTenanctEnabled(false);
-        systemSettings.setName("Test system");
-        systemSettings.setSystemIp("10.64.125.131");
-        systemSettings.setUser("admin");
-        systemSettings.setPassword("admin");
-        return new ResponseEntity<>(systemSettings, HttpStatus.OK);
-    }*/
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/rest/systems/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SystemSettings> findSystem(@PathVariable("id") Long id) {
+		log.debug("Testing systemSettings with id {}", id);
+		SystemSettings systemSettings = systemConnectionInfoRepository
+				.findOne(id);
+		addVirtualConfigurationInfo(systemSettings);
+		return new ResponseEntity<>(systemSettings, HttpStatus.OK);
+	}
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/rest/testSystem/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<SystemSettings>> testSystem(@PathVariable("id") Long id) {
-        log.debug("Testing systemSettings with id {}", id);
-        SystemSettings systemSettings = systemConnectionInfoRepository.findOne(id);
-        Client client = new Client(systemSettings);
-        client.getSystemTime();
-        systemConnectionInfoRepository.saveAndFlush(systemSettings);
-        return Optional.ofNullable(systemConnectionInfoRepository.findAll())
-        .map(clusterSettings -> new ResponseEntity<>(clusterSettings, HttpStatus.OK))
-        .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+	/*
+	 * @PreAuthorize("hasAuthority('ADMIN')")
+	 * 
+	 * @RequestMapping(value = "/rest/systems/{id}", method = RequestMethod.GET,
+	 * produces = MediaType.APPLICATION_JSON_VALUE) public
+	 * ResponseEntity<SystemSettings> findSystem(@PathVariable("id") Long id) {
+	 * log.debug("Testing systemSettings with id {}", id); SystemSettings
+	 * systemSettings = new SystemSettings(); systemSettings.setIsDrttc(true);
+	 * systemSettings.setIsMultiTenanctEnabled(false);
+	 * systemSettings.setName("Test system");
+	 * systemSettings.setSystemIp("10.64.125.131");
+	 * systemSettings.setUser("admin"); systemSettings.setPassword("admin");
+	 * return new ResponseEntity<>(systemSettings, HttpStatus.OK); }
+	 */
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/rest/addSystem", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<SystemSettings>> addNewSystem(
-    @RequestBody SystemSettings systemSettings) {
-        log.debug("Adding new systemSettings to the system {}", systemSettings);
-        validateNewSystem(systemSettings);
-        validateSystemData(systemSettings);
-        propagateClusterData(systemSettings);
-        validateNewClusters(systemSettings);
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/rest/testSystem/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<SystemSettings>> testSystem(
+			@PathVariable("id") Long id) {
+		log.debug("Testing systemSettings with id {}", id);
+		SystemSettings systemSettings = systemConnectionInfoRepository
+				.findOne(id);
+		Client client = new Client(systemSettings);
+		client.getSystemTime();
+		systemConnectionInfoRepository.saveAndFlush(systemSettings);
+		return Optional
+				.ofNullable(systemConnectionInfoRepository.findAll())
+				.map(clusterSettings -> new ResponseEntity<>(clusterSettings,
+						HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
 
-        systemConnectionInfoRepository.saveAndFlush(systemSettings);
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/rest/addSystem", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<SystemSettings>> addNewSystem(
+			@RequestBody SystemSettings systemSettings) {
+		log.debug("Adding new systemSettings to the system {}", systemSettings);
+		validateNewSystem(systemSettings);
+		validateSystemData(systemSettings);
+		propagateClusterData(systemSettings);
+		validateNewClusters(systemSettings);
 
-        log.debug("Returning list of all clusters");
-        return Optional.ofNullable(systemConnectionInfoRepository.findAll())
-        .map(clusterSettings -> new ResponseEntity<>(clusterSettings, HttpStatus.OK))
-        .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+		systemConnectionInfoRepository.saveAndFlush(systemSettings);
 
-    private void validateNewClusters(SystemSettings systemSettings) {
-        for (ClusterSettings cluster : systemSettings.getClusters()) {
-            List<ClusterSettings> results = systemConnectionInfoRepository
-            .findCluster(cluster.getClusterId());
-            if (!results.isEmpty()) {
-                String error = String.format(
-                "Only a single vRPA cluster can be added per RecoverPoint for VM system. \n"
-                + "The other cluster(s) are added automatically by the system");
-                log.warn("Entry for cluster {} exists already in system {}", cluster.getClusterId(),
-                cluster.getSystemSettings().getSystemIp());
-                throw new RpspException(error);
-            }
-        }
-    }
+		log.debug("Returning list of all clusters");
+		return Optional
+				.ofNullable(systemConnectionInfoRepository.findAll())
+				.map(clusterSettings -> new ResponseEntity<>(clusterSettings,
+						HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
 
-    private void propagateClusterData(SystemSettings systemSettings) {
-        Client client = new Client(systemSettings, systemConnectionInfoRepository);
-        client.getSystemTime();
-        Map<Long, String> clusters = client.getClusterNames();
-        for (Map.Entry<Long, String> entry : clusters.entrySet()) {
-            ClusterSettings cluster = new ClusterSettings(entry.getKey(), entry.getValue(),
-            systemSettings);
-            cluster.setFriendlyName(entry.getValue());
-            systemSettings.addCluster(cluster);
-        }
-    }
-    
-    
- /*   private void propagateClusterData(SystemSettings systemSettings) {
-    	
-    	ClusterSettings cluster = new ClusterSettings(2398086092600946265l, "NY-orig-name",
-                systemSettings);
-    	systemSettings.addCluster(cluster);
-    	
-    	cluster = new ClusterSettings(3794617345752337726l, "London-orig-name",
-                systemSettings);
-    	systemSettings.addCluster(cluster);
-    }*/
+	private void validateNewClusters(SystemSettings systemSettings) {
+		for (ClusterSettings cluster : systemSettings.getClusters()) {
+			List<ClusterSettings> results = systemConnectionInfoRepository
+					.findCluster(cluster.getClusterId());
+			if (!results.isEmpty()) {
+				String error = String
+						.format("Only a single vRPA cluster can be added per RecoverPoint for VM system. \n"
+								+ "The other cluster(s) are added automatically by the system");
+				log.warn("Entry for cluster {} exists already in system {}",
+						cluster.getClusterId(), cluster.getSystemSettings()
+								.getSystemIp());
+				throw new RpspException(error);
+			}
+		}
+	}
 
-    private void validateNewSystem(SystemSettings cluster) {
-        List<SystemSettings> clustersWithIp = systemConnectionInfoRepository
-        .findClustersWithIpAddress(cluster.getSystemIp());
-        if (clustersWithIp.size() > 0) {
-            log.error("Entry with IP {} exists already", cluster.getSystemIp());
-            throw new RpspException("Entry with IP " + cluster.getSystemIp() + " exists already");
-        }
-    }
+	private void propagateClusterData(SystemSettings systemSettings) {
+		Client client = new Client(systemSettings,
+				systemConnectionInfoRepository);
+		client.getSystemTime();
+		Map<Long, String> clusters = client.getClusterNames();
+		for (Map.Entry<Long, String> entry : clusters.entrySet()) {
+			ClusterSettings cluster = new ClusterSettings(entry.getKey(),
+					entry.getValue(), systemSettings);
+			cluster.setFriendlyName(entry.getValue());
+			systemSettings.addCluster(cluster);
+		}
+	}
 
-    private void validateSystemData(SystemSettings settings) {
-        if (!InetAddressUtils.isIPv4Address(settings.getSystemIp())) {
-            if (InetAddressUtils.isIPv6Address(settings.getSystemIp())) {
-                settings.setSystemIp("[" + settings.getSystemIp() + "]");
-            } else {
-                log.error("Not a valid IP address for cluster {}", settings);
-                throw new RpspException("Not a valid IP address");
-            }
-        }
-    }
+	/*
+	 * private void propagateClusterData(SystemSettings systemSettings) {
+	 * 
+	 * ClusterSettings cluster = new ClusterSettings(2398086092600946265l,
+	 * "NY-orig-name", systemSettings); systemSettings.addCluster(cluster);
+	 * 
+	 * cluster = new ClusterSettings(3794617345752337726l, "London-orig-name",
+	 * systemSettings); systemSettings.addCluster(cluster); }
+	 */
+
+	private void validateNewSystem(SystemSettings cluster) {
+		List<SystemSettings> clustersWithIp = systemConnectionInfoRepository
+				.findClustersWithIpAddress(cluster.getSystemIp());
+		if (clustersWithIp.size() > 0) {
+			log.error("Entry with IP {} exists already", cluster.getSystemIp());
+			throw new RpspException("Entry with IP " + cluster.getSystemIp()
+					+ " exists already");
+		}
+	}
+
+	private void validateSystemData(SystemSettings settings) {
+		if (!InetAddressUtils.isIPv4Address(settings.getSystemIp())) {
+			if (InetAddressUtils.isIPv6Address(settings.getSystemIp())) {
+				settings.setSystemIp("[" + settings.getSystemIp() + "]");
+			} else {
+				log.error("Not a valid IP address for cluster {}", settings);
+				throw new RpspException("Not a valid IP address");
+			}
+		}
+	}
+	
+	private void addVirtualConfigurationInfo(List<SystemSettings> systemSettingsList){
+		for(SystemSettings systemSettings : systemSettingsList){
+			addVirtualConfigurationInfo(systemSettings);
+		}
+	}
+	
+	private void addVirtualConfigurationInfo(SystemSettings systemSettings){
+		List<ClusterSettings> clusters = systemSettings.getClusters();
+		for(ClusterSettings cluster : clusters){
+			VcenterConfig vcenterConfig = virtualConfigurationService.getVirtualConfiguration(cluster.getClusterId());
+			cluster.setVcenterConfig(vcenterConfig);
+		}
+	}
 }
