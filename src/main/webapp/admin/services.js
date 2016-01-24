5f3dbde8c53050cc56953c32e680fc78ce5aef81
unknown <morand3@isenmorand3l1c.corp.emc.com>
2016-01-24 16:25:45 +0200

    json presenting changes on systems tab

--- src/main/webapp/admin/services.js
+++ src/main/webapp/admin/services.js
@@ -1,15 +1,26 @@
-angular.module('adminApp')
-    .service('RPSP', ['$http', '$q',
-    function ($http, $q) {
+(function () {
+    angular.module('adminApp')
+        .service('RPSP', ['$http', '$q', RPSP]);
+    function RPSP($http, $q) {
+        var json = {};
         return {
-            settings: function () {
-                return $q(function (resolve, reject) {
-                    setTimeout(function () {
-                        $http.get('data/settings.json').then(function (res) {
-                            resolve(res);
-                        });
-                    }, 1000);
-                });
-            }
+            settings: settings,
+            current: current
         }
-    }]);
+
+        function settings() {
+            return $q(function (resolve, reject) {
+                setTimeout(function () {
+                    $http.get('data/settings.json').then(function (res) {
+                        json = res.data;
+                        resolve(res);
+                    });
+                }, 1);
+            });
+        }
+
+        function current() {
+            return json;
+        }
+    }
+})()
--- src/main/webapp/admin/views/systems/systems.ctrl.js
+++ src/main/webapp/admin/views/systems/systems.ctrl.js
@@ -1,10 +1,12 @@
-angular.module('adminApp')
-    .controller('SystemsCtrl', ['$scope', 'RPSP',
-        function ($scope, RPSP) {
-            $scope.loading = true;
-            RPSP.settings().then(function (res) {
-                $scope.config = res.data.config;
-                $scope.loading = false;
-            })
-
-        }]);
+(function () {
+    angular.module('adminApp')
+        .controller('SystemsCtrl', ['$scope', 'RPSP', SystemsCtrl]);
+    function SystemsCtrl($scope, RPSP) {
+        $scope.loading = true;
+        RPSP.settings().then(function (res) {
+            $scope.config = res.data.config;
+            $scope.loading = false;
+            $scope.json = RPSP.current();
+        })
+    }
+})()
