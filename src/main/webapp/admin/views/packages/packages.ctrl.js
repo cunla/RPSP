4963e028d41a5b687cb4ad539827a48068c75947
Boris Shpilyuck <boris.shpilyuck@emc.com>
2016-01-28 18:52:23 +0200

    Packages UI - added controls to dialog

--- src/main/webapp/admin/views/packages/packages.ctrl.js
+++ src/main/webapp/admin/views/packages/packages.ctrl.js
@@ -5,6 +5,13 @@
     function PackagesCtrl($scope, RPSP, $mdDialog) {
         $scope.loading = true;
         $scope.showDialog=showDialog;
+        $scope.items = [
+                        { id: 1, name: 'Test_System_1' },
+                        { id: 2, name: 'Test_System_2' },
+                        { id: 3, name: 'Test_System_3' }
+                      ];
+        $scope.selectedItem = $scope.items[1];
+        
         RPSP.settings().then(function (res) {
             $scope.config = res.data;
             $scope.loading = false;
