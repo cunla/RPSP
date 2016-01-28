fb94fd97fb1c01ab40abe8fce62389cc4085a11a
Boris Shpilyuck <boris.shpilyuck@emc.com>
2016-01-28 19:24:44 +0200

    Data loader - added virtual config info

--- src/main/java/com/emc/rpsp/dataloader/controller/DataLoaderController.java
+++ src/main/java/com/emc/rpsp/dataloader/controller/DataLoaderController.java
@@ -1,22 +1,7 @@
 package com.emc.rpsp.dataloader.controller;
 
-import com.emc.rpsp.accounts.domain.Account;
-import com.emc.rpsp.accounts.service.AccountService;
-import com.emc.rpsp.dataloader.domain.InternalData;
-import com.emc.rpsp.dataloader.service.DataLoaderService;
-import com.emc.rpsp.fal.Client;
-import com.emc.rpsp.packages.domain.PackageConfig;
-import com.emc.rpsp.packages.domain.PackageDefinition;
-import com.emc.rpsp.packages.service.PackageDefinitionService;
-import com.emc.rpsp.rpsystems.ClusterSettings;
-import com.emc.rpsp.rpsystems.SystemConnectionInfoRepository;
-import com.emc.rpsp.rpsystems.SystemSettings;
-import com.emc.rpsp.users.domain.User;
-import com.emc.rpsp.users.service.UserService;
-import com.emc.rpsp.vms.domain.VmOwnership;
-import com.emc.rpsp.vms.service.VmOwnershipService;
+import java.util.List;
 
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,23 +11,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.emc.rpsp.dataloader.domain.InternalData;
+import com.emc.rpsp.dataloader.service.DataLoaderService;
+import com.emc.rpsp.rpsystems.ClusterSettings;
+import com.emc.rpsp.rpsystems.SystemSettings;
+import com.emc.rpsp.virtualconfig.domain.VcenterConfig;
+import com.emc.rpsp.virtualconfig.service.VirtualConfigurationService;
 
 @Controller
 public class DataLoaderController {
 
 	@Autowired
 	private DataLoaderService dataLoaderService;
+		
+	@Autowired
+	private VirtualConfigurationService virtualConfigurationService;
+	
 
 	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/internal-data", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<InternalData> getInternalData() {
+	public ResponseEntity<InternalData> getInternalData(@RequestParam(value = "includeVirtualConfig", required=false, defaultValue = "false") Boolean includeVirtualConfig) {
 		InternalData internalData = dataLoaderService.getInternalData();
+		if(includeVirtualConfig){
+			addVirtualConfigurationInfo(internalData.getSystems());
+		}
 		return new ResponseEntity<>(internalData, HttpStatus.OK);
 	}
 	
@@ -51,7 +45,8 @@ public class DataLoaderController {
 	@RequestMapping(value = "/internal-data", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<InternalData> populateInternalData(
 			@RequestBody InternalData internalData) {
-		InternalData res = dataLoaderService.populateInternalData(internalData);		
+		InternalData res = dataLoaderService.populateInternalData(internalData);	
+		
 		return new ResponseEntity<>(res, HttpStatus.CREATED);
 	}
 	
@@ -63,6 +58,21 @@ public class DataLoaderController {
 		return new ResponseEntity<>(template, HttpStatus.OK);
 	}
 	
+	
+	
+	private void addVirtualConfigurationInfo(List<SystemSettings> systemSettingsList){
+		for(SystemSettings systemSettings : systemSettingsList){
+			addVirtualConfigurationInfo(systemSettings);
+		}
+	}
+	
+	private void addVirtualConfigurationInfo(SystemSettings systemSettings){
+		List<ClusterSettings> clusters = systemSettings.getClusters();
+		for(ClusterSettings cluster : clusters){
+			VcenterConfig vcenterConfig = virtualConfigurationService.getVirtualConfiguration(cluster.getClusterId());
+			cluster.setVcenterConfig(vcenterConfig);
+		}
+	}
 
 
 }
