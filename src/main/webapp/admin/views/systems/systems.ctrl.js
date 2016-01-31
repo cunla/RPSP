ef442347ea42f0b6a7fed71d5c8f11b32bb93e78
unknown <morand3@isenmorand3l1c.corp.emc.com>
2016-01-31 13:28:06 +0200

    added buttons

--- src/main/webapp/admin/views/systems/systems.ctrl.js
+++ src/main/webapp/admin/views/systems/systems.ctrl.js
@@ -2,20 +2,34 @@
     angular.module('adminApp')
         .controller('SystemsCtrl', ['$scope', 'RPSP', '$mdDialog', SystemsCtrl])
     function SystemsCtrl($scope, RPSP, $mdDialog) {
-        $scope.menuOpen=false;
+        $scope.menuOpen = false;
         $scope.showDialog = showDialog;
+        $scope.testSystem = testSystem;
+        $scope.save=save;
         $scope.loading = true;
+        $scope.changed=false;
         refresh();
+
+
+        function save(){
+            RPSP.save($scope.json).then(function(res){
+                $scope.changed=false;
+            })
+        }
 
         function refresh() {
             RPSP.settings().then(function (res) {
-                $scope.config = res.data;
+                $scope.config = RPSP.current();
                 $scope.loading = false;
-                $scope.json = RPSP.current();
+                //$scope.json = RPSP.current();
             });
         }
 
-        function showDialog($event) {
+        function showDialog(system) {
+            $scope.changed=true;
+            if(!system){
+                system = {};
+            }
             var parentEl = angular.element(document.body);
             $mdDialog.show({
                 templateUrl: 'views/systems/newSystemDialog.html',
@@ -30,6 +44,13 @@
                     $mdDialog.hide();
                 }
             }
+        }
+
+        function testSystem(system) {
+            system.testing = true;
+            RPSP.testSystem(system.id).then(function (data) {
+                system.testing = false;
+            })
         }
     }
 })()
