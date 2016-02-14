8a9427e962342a97a7b2552a2df6876c1f94aaa5
Boris Shpilyuck <boris.shpilyuck@emc.com>
2016-02-14 14:44:50 +0200

    Exception info is written to log

--- src/main/java/com/emc/rpsp/config/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/emc/rpsp/config/GlobalDefaultExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.emc.rpsp.config;
 
-import com.emc.rpsp.ExceptionInfo;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,16 +11,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.servlet.http.HttpServletRequest;
+import com.emc.rpsp.ExceptionInfo;
 
 /**
  * Created by morand3 on 11/12/2015.
  */
 @ControllerAdvice class GlobalDefaultExceptionHandler {
     public static final String DEFAULT_ERROR_VIEW = "error";
+    private final Logger log = LoggerFactory.getLogger(GlobalDefaultExceptionHandler.class);
 
     @ResponseStatus(HttpStatus.BAD_REQUEST) @ExceptionHandler(Exception.class) @ResponseBody
     ExceptionInfo defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+    	log.error(e.getMessage(), e);
         // If exception is part of the
         if (null != AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class))
             throw e;
