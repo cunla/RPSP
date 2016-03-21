73f07e2794665cb0de70d771649f01461da5f6c1
unknown <morand3@isenmorand3l1c.corp.emc.com>
2016-03-21 09:26:40 +0200

    started working on import/export

--- src/main/webapp/admin/views/tools/tools.js
+++ src/main/webapp/admin/views/tools/tools.js
@@ -1,20 +1,16 @@
-'use strict';
+(function () {
+    'use strict';
 
-/**
- * @ngdoc function
- * @name ngmaterialApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the ngmaterialApp
- */
-angular.module('adminApp')
-    .controller('ToolsCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Angular Material',
-            'Karma',
-            'Angular UI Router',
-            'Sample Login Screen'
-        ];
-    });
+    /**
+     * @ngdoc function
+     * @name ngmaterialApp.controller:AboutCtrl
+     * @description
+     * # AboutCtrl
+     * Controller of the ngmaterialApp
+     */
+    angular.module('adminApp')
+        .controller('ToolsCtrl', [ToolsCtrl]);
+    function ToolsCtrl($scope) {
+
+    }
+})()
