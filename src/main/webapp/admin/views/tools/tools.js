76ffa393d7f15fad3414620a9bbb49e57a4bfe13
unknown <morand3@isenmorand3l1c.corp.emc.com>
2016-03-21 09:53:35 +0200

    export working

--- src/main/webapp/admin/views/tools/tools.js
+++ src/main/webapp/admin/views/tools/tools.js
@@ -9,8 +9,26 @@
      * Controller of the ngmaterialApp
      */
     angular.module('adminApp')
-        .controller('ToolsCtrl', [ToolsCtrl]);
-    function ToolsCtrl($scope) {
+        .config(['$compileProvider',
+            function ($compileProvider) {
+                $compileProvider.aHrefSanitizationWhitelist(/^s*(https?|ftp|blob|mailto|chrome-extension):/);
+                // pre-Angularv1.2 use urlSanizationWhitelist()
+            }
+        ])
+        .controller('ToolsCtrl', ['RPSP', '$scope', ToolsCtrl]);
+    function ToolsCtrl(RPSP, $scope) {
+        RPSP.settings().then(function (res) {
+            $scope.json = RPSP.current();
+            var jsonStr = JSON.stringify($scope.settings);
+            $scope.settingsLink =
+                window.URL.createObjectURL(new Blob([jsonStr], {type: "application/json"}));
 
+        });
+
+        $scope.import = doImport;
+
+        function doImport() {
+
+        }
     }
 })()
